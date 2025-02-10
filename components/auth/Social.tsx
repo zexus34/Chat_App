9886b4befca9585eeb4ab3168231aa3b81e303b7
Ankit Jha <ajo91277@gmail.com>
2025-02-11 01:41:27 +0530

    Enhance social authentication buttons with loading state; add framer-motion for improved UI feedback

--- components/auth/Social.tsx
+++ components/auth/Social.tsx
@@ -5,12 +5,18 @@ import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { useState } from "react";
 
 const Social = () => {
-  const onClick = (provider: "google" | "github") => {
-    signIn(provider, {
+  const [loadingProvider, setLoadingProvider] = useState<
+    "google" | "github" | null
+  >(null);
+  const onClick = async (provider: "google" | "github") => {
+    setLoadingProvider(provider);
+    await signIn(provider, {
       redirectTo: DEFAULT_LOGIN_REDIRECT,
     });
+    setLoadingProvider(null);
   };
   return (
     <div className="flex flex-col items-center w-full space-y-2">
@@ -18,17 +24,27 @@ const Social = () => {
         size="lg"
         className="w-full"
         variant="outline"
+        disabled={!!loadingProvider}
         onClick={() => onClick("google")}
       >
-        <FcGoogle className="h-5" />
+        {loadingProvider === "google" ? (
+          <div className="w-5 h-5 border-2 border-gray-500 border-t-transparent rounded-full animate-spin"></div>
+        ) : (
+          <FcGoogle className="h-5 w-5" />
+        )}
       </Button>
       <Button
         size="lg"
         className="w-full"
         variant="outline"
+        disabled={!!loadingProvider}
         onClick={() => onClick("github")}
       >
-        <FaGithub className="h-5" />
+        {loadingProvider === "github" ? (
+          <div className="w-5 h-5 border-2 border-gray-500 border-t-transparent rounded-full animate-spin"></div>
+        ) : (
+          <FaGithub className="h-5 w-5" />
+        )}
       </Button>
     </div>
   );
