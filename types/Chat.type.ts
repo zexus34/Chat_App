80ccf925f65adde5c5b9eb353a258dae7ee4f54f
Ankit Jha <ajo91277@gmail.com>
2025-02-21 22:40:38 +0530

    refactor: remove chat and message models along with related types

--- types/Chat.type.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-export interface ChatType extends mongoose.Document<mongoose.Types.ObjectId> {
-  name: string;
-  isGroupChat: boolean;
-  lastMessage?: mongoose.Types.ObjectId;
-  participants: mongoose.Types.ObjectId[];
-  admin: mongoose.Types.ObjectId;
-}
