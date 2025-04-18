62d134f1d64c2e49a14c4481ed3ad68b26d864e1
Ankit Jha <ajo91277@gmail.com>
2025-04-19 03:00:26 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="overflow-hidden" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta
           name="viewport"
