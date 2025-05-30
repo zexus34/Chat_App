2890e15394cab43807d43172da3aaceffba3074c
Ankit Jha <ajo91277@gmail.com>
2025-05-30 20:59:31 +0530

    refactor: remove redundant comments and unused toJSON method from ApiResponse class

--- src/lib/api/ApiResponse.ts
+++ src/lib/api/ApiResponse.ts
@@ -11,24 +11,11 @@ interface ApiResponseProps<T = unknown> {
  * @template T - The type of the data being returned in the response.
  */
 class ApiResponse<T = unknown> {
-  /**
-   * The HTTP status code of the response.
-   */
   public readonly statusCode: number;
 
-  /**
-   * The data returned in the response. Can be null if no data is returned.
-   */
   public readonly data: T | null;
-
-  /**
-   * A message describing the response. Defaults to "Success".
-   */
   public readonly message: string;
 
-  /**
-   * Indicates whether the request was successful. Defaults to true if the status code is between 200 and 299.
-   */
   public readonly success: boolean;
 
   constructor({
@@ -42,20 +29,6 @@ class ApiResponse<T = unknown> {
     this.message = message;
     this.success =
       success !== undefined ? success : statusCode >= 200 && statusCode < 300;
-  }
-
-  /**
-   * Converts the ApiResponse instance to a JSON object.
-   *
-   * @returns {Object} The JSON representation of the ApiResponse instance.
-   */
-  toJSON() {
-    return {
-      statusCode: this.statusCode,
-      message: this.message,
-      data: this.data,
-      success: this.success,
-    };
   }
 }
 
